f60dde66c7a9916e09fda16d56334f8d55ed9d56
Parag Panse <vagrant@ubuntu-xenial.none>
2020-06-20 07:15:29 +0000

    Simple tool to write a timeseries for test_metric.

---
+++ tools/http_post_using_json_simple_script.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''
+  Usage:
+    python3 http_post_using_json_simple_script.py
+
+  On Grafana:
+    http://34.221.154.248:3000/d/l3XTzKZMk/test_metric-dashboard?orgId=1
+'''
+
+import requests
+import json
+import time
+from random import randint
+from time import sleep
+
+def push_data():
+  url = 'http://34.221.154.248:4242/api/put'
+  headers = {'content-type': 'application/json'}
+  datapoint = {}
+  datapoint['metric'] = 'test_metric'
+  datapoint['timestamp'] = round(time.time())  # timestamp
+  datapoint['value'] = randint(3300, 3600)  # value
+  datapoint['tags'] = {}
+  datapoint['tags']['machine_id'] = 'cold_draw'
+  response = requests.post(url, data=json.dumps(datapoint), headers=headers)
+  return response, datapoint['timestamp']
+
+
+data_point_count = 100
+
+while(data_point_count > 0):
+  response, data_time_stamp = push_data()
+  print("%d Data point pushed. Result %s" % (data_time_stamp, str(response)))
+  sleep(randint(1,11))
+  data_point_count = data_point_count - 1
+
