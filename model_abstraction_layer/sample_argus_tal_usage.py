c21e3b9095a6af1675096446743ae16fbb1688b8
Parag Panse <vagrant@ubuntu-xenial.none>
2020-06-25 00:57:31 +0000

    Woot git add sample_argus_tal_usage.py  First usage of argus_tal query API.

--- model_abstraction_layer/sample_argus_tal_usage.py
+++ model_abstraction_layer/sample_argus_tal_usage.py
@@ -1,8 +1,36 @@
 #!/usr/bin/python3
+'''
+   FIXME: Add sections to this sample application to demonstrate various
+          features of the argus_tal query API.
+'''
 
 from argus_tal import query_api
+from argus_tal import timeseries_id as ts_id
+from argus_tal import timestamp as ts
+from argus_tal import basic_types as bt
+from argus_tal import query_urlgen as qurlgen
+from argus_tal import timeseries_datadict as tsd
 
-foo = query_api.QueryApi()
-assert foo.hello() == "Hello from QueryApi"
+start_timestamp = ts.Timestamp(1593043062)
+end_timestamp = ts.Timestamp(1593043117)
 
-print("Using argus_tal package works !")
+timeseries_id = ts_id.TimeseriesID("machine.sensor.raw_melt_temperature", {})
+
+foo = query_api.QueryApi(
+        "34.221.154.248", 4242, \
+        start_timestamp, end_timestamp, \
+        [timeseries_id], \
+        bt.Aggregator.NONE \
+      )
+
+rv = foo.populate_ts_data()
+assert rv == 0
+
+result_list = foo.get_result_set()
+print("Number of timeseries retrieved -- %d" % len(result_list))
+for result in result_list:
+  print("Retrieved timeseries id : %s" % \
+         str(result.get_timeseries_id()))
+  print("Datapoints:")
+  for kk, vv in result:
+    print("\t%s->%d" % (kk, vv))
