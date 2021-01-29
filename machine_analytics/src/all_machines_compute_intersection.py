dfb8d3aecdb521ee02e0c530536a95ec15fbd5e1
Parag Panse <paragpm@argusanalytics.io>
2021-01-29 19:58:11 +0000

    Added the subsume case. Made changes to improve readability so that algorithm is easily understood by code readers.

--- machine_analytics/src/all_machines_compute_intersection.py
+++ machine_analytics/src/all_machines_compute_intersection.py
@@ -9,6 +9,16 @@
 from all_machines_filter_primitive import FilteredTimeseries
 
 class IntersectTimeseries(object):
+
+    '''
+    TimeWindowId is used to annotate the L- and R- residues returned by
+    _compare_time_windows. This use of an enum here is primarily for making
+    code more readable so that the algorithm is easily understood.
+    '''
+    class TimeWindowId(Enum):
+        W1 = 1,
+        W2 = 2
+
     def __init__(self, filterd_ts_obj_list):
         for obj in filtered_ts_obj_list:
             assert isinstance(obj) == FilteredTimeseries
@@ -22,11 +32,12 @@ class IntersectTimeseries(object):
         ----------
         Compare 2 time windows determine how much overlap exists between the
         supplied time windows. The method returns:
-          -- 3 time windows viz.  L-Residue, Overlap, R-Residue 
+          -- 3 time windows viz.  L-Residue, Overlap, R-Residue
              See details below for their semantics.
-          -- residue_window: a boolean value set to:
-             True: If the L-Residue window comes from window1
-             False: If the L-Residue window comes from window2
+          -- r_residue_in_window_1:
+             A tri-state boolean value set to:
+             True: If the R-Residue window comes from window1
+             False: If the R-Residue window comes from window2
              None: If there's exact overlap
 
         Input:
@@ -36,80 +47,99 @@ class IntersectTimeseries(object):
                                  w1 = (st1, et1)
                                  w2 = (st2, et2)
 
-        Result semantics:
-        -----------------
-        Possible overlap patterns between w1 and w2. Note that we're only
-        looking at temporal overlap here between the 2 windows of time.
-
+        Patterns being sought out:
+        --------------------------
+        Comparing 2 time windows, only 4 possibilities exist:
             Case 1: Full overlap
-                                 w1  *************
-
-                                 w2  *************
-
             Case 2: Partial overlap
-                        Case2a              |                Case2b
-            w1          *************       | w1 *************
-                                            |
-            w2  *************               | w2        *************
-
-
             Case 3: No overlap
-                        Case3a              |                Case3b
-            w1                ************* | w1 *************
-                                            |
-            w2 *************                | w2                 *************
+            Case 4: Subsume
+        This method identifies which case is met by the supplied time windows.
+        See ASCII art patterns below.
 
-        Output:
+        Return:
         -------
-            3 time windows dubbed as L-Residue, Overlap & R-Residue
+        A tuple of 3 time windows: L-Residue, Overlap, R-Residue
 
-            L-Residue: Residue to the left of the overlapping time window.
-            Overlap: Overlapping time window.
-            R-Residue: Residue to the right of the overlapping time window.
+        L-Residue: Residue to the left of the overlapping time window.
+        Overlap: Overlapping time window.
+        R-Residue: Residue to the right of the overlapping time window.
+
+        Each of the 3 time windows are themselves returned as tuples of the
+        following format:
+           Overlap window: (start_time, end_time)
+           Residue windows: (start_time, end_time, time_window_identifier)
         '''
         st1, et1 = window1
         st2, et2 = window2
         l_residue = overlap = r_residue = None
-        residue_window = None
+        r_residue_in_window_1 = None
 
         if st1 == st2 and et1 == et2:
-            # This handles the Full Overlap case.
+            ''' This handles Case 1 - Full Overlap. '''
             overlap = (st1, et1)
         else:
-            # We're now only dealing with Partial Overlap or No Overlap cases.
+            ''' ...this handles all the remaining cases. '''
             input_windows = [st1, et1, st2, et2]
             input_windows.sort()
 
+            '''
+            Timestamp pattern we expect to find in the sorted input_windows
+            array.
+
+            Case 2a: Partial overlap
+                      st1 ************* et1
+              st2 ************ et2
+
+            Case 2b: Partial overlap
+              st1 ************* et1
+                     st2 ************ et2
+
+            Case 3a: No overlap
+                                    st1 ************* et1
+              st2 ************ et2
+
+            Case 3b: No overlap
+              st1 ************* et1
+                                       st2 ************ et2
+
+            Case 4a: Subsume
+              st1 *************************** et1
+                       st2 ************ et2
+
+            Case 4a: Subsume
+                       st1 ************ et1
+              st2 *************************** et2
+            '''
+            # shorten name for readability
+            TWindow = IntersectTimeseries.TimeWindowId
+
             if input_windows == [st2, st1, et2, et1]:   # Case 2a
-                #         st1 ************* et1
-                # st2 ************ et2
-                l_residue = (st2, st1)
+                l_residue = (st2, st1, TWindow.W2)
                 overlap = (st1, et2)
-                r_residue = (et2, et1)
-                residue_window = False
-            elif input_windows == [st2, st1, et2, et1]: # Case 2b
-                # st1 ************* et1
-                #        st2 ************ et2
-                l_residue = (st1, st2)
+                r_residue = (et2, et1, TWindow.W1)
+            elif input_windows == [st1, st2, et1, et2]: # Case 2b
+                l_residue = (st1, st2, TWindow.W1)
                 overlap = (st2, et1)
-                r_residue = (et1, et2)
-                residue_window = True
-            elif input_windows == [st2, st1, et2, et1]: # Case 3a
-                #                        st1 ************* et1
-                # st2 ************ et2
-                l_residue = (st2, et2)
-                r_residue = (st1, et1)
-                residue_window = False
+                r_residue = (et1, et2, TWindow.W2)
+            elif input_windows == [st2, et2, st1, et1]: # Case 3a
+                l_residue = (st2, et2, TWindow.W2)
+                r_residue = (st1, et1, TWindow.W1)
+            elif input_windows == [st1, et1, st2, et2]: # Case 3b
+                l_residue = (st1, et1, TWindow.W1)
+                r_residue = (st2, et2, TWindow.W2)
+            elif input_windows == [st1, st2, et2, et1]: # Case 4a
+                l_residue = (st1, st2, TWindow.W1)
+                overlap = (st2, et2)
+                r_residue = (et2, et1, TWindow.W1)
+            elif input_windows == [st2, st1, et1, et2]: # Case 4b
+                l_residue = (st2, st1, TWindow.W2)
+                overlap = (st1, et1)
+                r_residue = (et1, et2, TWindow.W2)
             else:
-            elif input_windows == [st2, st1, et2, et1]: # Case 3b
-                # st1 ************* et1
-                #                          st2 ************ et2
-                l_residue = (st1, et1)
-                r_residue = (st2, et2)
-                residue_window = True
                 assert(False)  # Should never happen
 
-        return l_residue, overlap, r_residue, residue_window
+        return l_residue, overlap, r_residue
 
 
     def compute(self):
