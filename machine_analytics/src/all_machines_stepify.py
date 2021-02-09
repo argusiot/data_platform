a29da49871ba78df4354b6b453cdfd3c597f8760
Vishwas Lokesh <vishwas@Vishwass-MacBook-Pro.local>
2021-02-09 11:23:58 -0800

    Applied changes from pull request and amoved timewindows to a different file

--- machine_analytics/src/all_machines_stepify.py
+++ machine_analytics/src/all_machines_stepify.py
@@ -1,30 +1,42 @@
 '''
-Stepify TBD
+    This class accepts as input a FilteredTimeseries object, which contains data points as
+    (time, value) pairs and also markers* which are special type of data points identifying a
+    boundary condition.
+
+    To plot data points on a graph, we'd put time as X-axis and value as Y-axis.
+
+    Purpose of Stepify is take the FilteredTimeseries as input and generate a
+    step function such that all Y-axis values in the step function are either 0 or
+    the threshold value. The threshold value is defined by
+    FilteredTimeseries::get_filter_constant().
+
+    *Marker type and the immediately preceding/following value are used to decide whether
+     to "step up" from 0 to 'threshold' or to "step down" from 'threshold' to 0. See __stepify()
+     below for details.
+
+     The resulting step function can be obtained by calling Stepify::get_stepified_time_windows().
+     It returns an object of the type TimeWindowSequence.
+
+     To visually understand how it works, consult the quilt algorithm:
+     https://docs.google.com/presentation/d/15EnUsMb4w9Xwg26iMBw8uNcUB_Sd5uAqXQWmie3X60s
 '''
 
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 from all_machines_filter_primitive import FilteredTimeseries
-
-
-class TimeWindows(object):
-    def __init__(self, tw):
-        self.__time_windows = tw
-
-    def get_time_windows(self):
-        return self.__time_windows
-
-    def set_time_windows(self, time_windows):
-        self.__time_windows = time_windows
+from all_machine_time_windows import TimeWindowSequence
 
 
 class Stepify(object):
     def __init__(self, filtered_ts):
+        assert(isinstance(filtered_ts, FilteredTimeseries))
         self.__filtered_timeseries = filtered_ts
+
         self.__transition_points = None
+        # self.__transition_points is used to store the 'transition points' computed during the stepify process.
+        # These transition_points are actually an intermediate result that are used to compute the time windows.
+
         self.__stepified_time_windows = None
+        # self.__stepified_time_windows is used to store the final result (as a list of TimeWindows objects)
+
         self.__stepify()
 
     def get_stepified_time_windows(self):
@@ -59,12 +71,21 @@ class Stepify(object):
         for idx, val in enumerate(self.__transition_points):
             if idx < len(self.__transition_points) - 1:
                 time_windows.append((val, self.__transition_points[idx+1]))
-        self.__stepified_time_windows = TimeWindows(time_windows)
+        self.__stepified_time_windows = TimeWindowSequence(time_windows)
 
     def __stepify(self):
         current_marker = self.__filtered_timeseries.get_first_marker()
         threshold = self.__filtered_timeseries.get_filter_constant()
         element_list = []
+
+        '''
+        The basic algorithm here is as follows:
+        Walk through all the markers:
+           At each marker, make a decision on 
+                 (a) whether a step should be generated
+                 (b) If yes, generate the step by computing the point using __get_interpolation_point()
+           Decision (a) is made based on the type of marker and what precedes/follows it.
+        '''
 
         while self.__filtered_timeseries.get_next_marker(current_marker) is not None:
             if current_marker.get_marker_type() == FilteredTimeseries.MarkerTypes.INIT:
