69ca8b545bb15722522ee7ea422e36c86624d23c
Ulf Stroberg <argus-iot@strobergs.net>
2021-09-01 18:05:29 -0700

    quilt-steel-poc: Added args for start, end and window
    In order to simplify runs for multiple dat files, applique args
    were added for start, end and quilt window size.
    
    Signed-off-by: Ulf Stroberg <argus-iot@strobergs.net>

--- quilt_demos/steel_poc_plate_thickness_applique.py
+++ quilt_demos/steel_poc_plate_thickness_applique.py
@@ -4,7 +4,7 @@
 import json
 import os
 import sys
-
+import argparse
 import argus_quilt
 import importlib.resources as pkg_resources
 
@@ -12,6 +12,20 @@ from argus_quilt.state_set_processor_builder import StateSetProcessorBuilder
 
 
 def main():
+
+    # Default start value is first sample of our first test dat file.
+    # Start: Saturday, February 13, 2021 7:01:01 AM GMT
+    dflt_start = 1613199661
+    dlft_win = 30
+
+    psr = argparse.ArgumentParser(description="ArgusIoT Applique Tool")
+    psr.add_argument("--start", type=int, default=dflt_start,
+                     help="Epoch time stamp for first point in time series")
+    psr.add_argument("--end", type=int, default=(dflt_start + 3600),
+                     help="Epoch time stamp for first point in time series")
+    psr.add_argument("--win", type=int, default=dflt_win,
+                     help=f"Number of samples in quit window {dflt_win}")
+    args = psr.parse_args()
 
     with pkg_resources.path( \
             "argus_quilt", "SCHEMA_DEFN_state_set.json") as schema_file:
@@ -23,13 +37,9 @@ def main():
             state_set_json_schema = json.load(file)
         processor = builder.build(state_set_json_schema)
 
-        # Start: Saturday, February 13, 2021 7:01:01 AM GMT
-        # End:   Saturday, February 13, 2021 8:01:00 AM GMT
-        # start = 1613199661
-        # end = 1613203260
-        start = 1613199661
-        end = 1613203260
-        computation_window_in_sec = 30
+        start = args.start
+        end = args.end
+        computation_window_in_sec = args.win
         processor.one_shot(start, end, computation_window_in_sec)
 
 
