bfe4822243ddeb339f1f48d5cb8b3a781c0c0169
Parag Panse <paragpm@argusanalytics.io>
2021-11-11 21:51:19 +0000

    Bumping up version number. Query result API enhancement to get result as a dataframe.

--- tsdb_abstraction_layer/setup.py
+++ tsdb_abstraction_layer/setup.py
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
 
 setup(
     name='argustal',
-    version='0.1.2',
+    version='0.1.3',
     description='Timeseries Abstraction Layer for ArgusIoT',
     long_description=readme,
     install_requires=['wheel'],
