bc92a3499d978bc551010a3f7b86c53f5b73ba39
Archan Das <archan.das@argusanalytics.io>
2022-06-06 14:12:07 -0500

    Revert "fixed quilt tags bug"
    This reverts commit 24e8a024733507995aa9dd97e51f089c2358d449.

--- quilt/integration_tests/it_main.py
+++ quilt/integration_tests/it_main.py
@@ -1,7 +1,6 @@
 import json
 import os
 import sys
-sys.path.append("..")
 
 import argus_quilt
 import importlib.resources as pkg_resources
@@ -13,7 +12,7 @@ def main():
 
     with pkg_resources.path( \
             "argus_quilt", "SCHEMA_DEFN_state_set.json") as schema_file:
-        builder = StateSetProcessorBuilder(schema_file, "localhost", 4242)
+        builder = StateSetProcessorBuilder(schema_file, "34.221.154.248", 4242)
         __location__ = os.path.realpath(os.path.join(os.getcwd(),
                                         os.path.dirname(__file__)))
         with open(os.path.join(__location__, "sample_input.json")) as file:
