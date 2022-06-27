503748c05f84c87654f704bf5ca7bbd18ea15b98
Archan Das <archan.das@argusanalytics.io>
2022-06-27 16:55:16 -0500

    made it_main.py into a full demo script

---
+++ quilt/integration_tests/grafana_dashboard_generator.py
@@ -0,0 +1,53 @@
+from grafanalib.core import (
+    Dashboard, TimeSeries, PieChartv2
+)
+from grafanalib.opentsdb import OpenTSDBTarget
+import json
+
+from numpy import DataSource
+
+from grafanalib._gen import print_dashboard
+
+
+
+class AppliqueDashboard:
+    def __init__(self, applique_file) -> None:
+        with open(applique_file) as app_file:
+            self.appliqueJSON = json.load(app_file)
+        self.input_ts_list = self.appliqueJSON['input_timeseries']
+        self.output_ts = self.appliqueJSON['output_timeseries_template']
+        self.state_set = [state_def['label'] for state_def in self.appliqueJSON['state_definitions']]
+
+    def __createDashboard(self):
+        panel_list = []
+        for ts in self.input_ts_list:
+            panel_list.append(self.__createTimeSeries(ts['ts_name'], ts['ts_defn']['metric'], ts['ts_defn']['tags']))
+        
+        panel_list.append(self.__createPieChart(self.appliqueJSON["name"]))
+
+        dashboard = Dashboard(
+            title=self.appliqueJSON["name"],
+            panels=panel_list
+        ).auto_panel_ids()
+        return dashboard
+
+    def __createTimeSeries(self, name, metric, tags):
+        return TimeSeries(title=name, dataSource="default", targets=[OpenTSDBTarget(metric=metric, filters=tags)])
+    
+    def __createPieChart(self, name):
+        filter_tags = self.output_ts['tags']
+        filtersets = []
+        for state in self.state_set:
+            state_filter = filter_tags
+            state_filter["state_label"] = state
+            filtersets.append(state_filter)
+        
+        targets = [OpenTSDBTarget(metric=self.output_ts["metric"], filters = st_filter) for st_filter in filtersets]
+        return PieChartv2(title=name, dataSource="default", targets=targets)
+    
+    def writeDashboard(self):
+        dashboard = self.__createDashboard()
+        print_dashboard(dashboard)
+
+adash = AppliqueDashboard("sample_input.json")
+adash.writeDashboard()
--- quilt/integration_tests/it_main.py
+++ quilt/integration_tests/it_main.py
@@ -1,14 +1,21 @@
+from cgi import test
 import json
 import os
 import sys
+<<<<<<< HEAD
 sys.path.append("..")
+=======
+sys.path.append("../../..")
+>>>>>>> 03c59c5 (made it_main.py into a full demo script)
 import argus_quilt
 import importlib.resources as pkg_resources
-
+import test_data_generator
+from grafana_dashboard_generator import AppliqueDashboard
 from argus_quilt.state_set_processor_builder import StateSetProcessorBuilder
 
 
 def main():
+    test_data_generator.main()
 
     with pkg_resources.path( \
             "argus_quilt", "SCHEMA_DEFN_state_set.json") as schema_file:
@@ -19,6 +26,10 @@ def main():
             state_set_json_schema = json.load(file)
         processor = builder.build(state_set_json_schema)
         processor.one_shot(1616083200, 1616083360, 30)
+    
+    adash = AppliqueDashboard("sample_input.json")
+    adash.upload_to_grafana("2021-03-18T15:59:45.000Z", "2021-03-18T16:03:00.000Z", "localhost:3000")
+
 
 
 main()
