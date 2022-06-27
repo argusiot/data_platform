4a1ae5b7ce110642400bdfa51be540875f08286f
Archan Das <archan.das@argusanalytics.io>
2022-06-27 16:55:16 -0500

    made it_main.py into a full demo script

--- quilt/integration_tests/grafana_dashboard_generator.py
+++ quilt/integration_tests/grafana_dashboard_generator.py
@@ -1,12 +1,12 @@
 from grafanalib.core import (
-    Dashboard, TimeSeries, PieChartv2
+    Dashboard, TimeSeries, PieChartv2, GridPos, Time
 )
-from grafanalib.opentsdb import OpenTSDBTarget
-import json
+from grafanalib.opentsdb import OpenTSDBTarget, OpenTSDBFilter
+import json, requests
 
 from numpy import DataSource
 
-from grafanalib._gen import print_dashboard
+from grafanalib._gen import print_dashboard, DashboardEncoder
 
 
 
@@ -18,34 +18,71 @@ class AppliqueDashboard:
         self.output_ts = self.appliqueJSON['output_timeseries_template']
         self.state_set = [state_def['label'] for state_def in self.appliqueJSON['state_definitions']]
 
-    def __createDashboard(self):
+    def __createDashboard(self, time):
         panel_list = []
-        for ts in self.input_ts_list:
-            panel_list.append(self.__createTimeSeries(ts['ts_name'], ts['ts_defn']['metric'], ts['ts_defn']['tags']))
+        panel_list.append(self.__createTimeSeries("Input Timeseries"))
         
         panel_list.append(self.__createPieChart(self.appliqueJSON["name"]))
 
         dashboard = Dashboard(
             title=self.appliqueJSON["name"],
-            panels=panel_list
+            panels=panel_list,
+            time=time
         ).auto_panel_ids()
         return dashboard
 
-    def __createTimeSeries(self, name, metric, tags):
-        return TimeSeries(title=name, dataSource="default", targets=[OpenTSDBTarget(metric=metric, filters=tags)])
+    def __createTimeSeries(self, name):
+        targets = []
+        for ts in self.input_ts_list:
+            filters = []
+            for k, v in ts["ts_defn"]["tags"].items():
+                filters.append(OpenTSDBFilter(value=v, tag=k, type="literal_or"))
+            targets.append(OpenTSDBTarget(metric=ts["ts_defn"]["metric"], aggregator=None, filters=filters))
+        return TimeSeries(title=name, dataSource="default", targets=targets, gridPos=GridPos(h=12, w=20, x=0, y=0))
     
     def __createPieChart(self, name):
         filter_tags = self.output_ts['tags']
-        filtersets = []
+        metric = self.output_ts["metric"]
+        targets = []
         for state in self.state_set:
             state_filter = filter_tags
             state_filter["state"] = state
             filtersets.append(state_filter)
         
-        targets = [OpenTSDBTarget(metric=self.output_ts["metric"], filters = st_filter) for st_filter in filtersets]
-        return PieChartv2(title=name, dataSource="default", targets=targets)
+        return PieChartv2(title=name, dataSource="default", targets=targets, gridPos=GridPos(h=8, w=16, x=0, y=8), reduceOptionsCalcs=["sum"])
     
-    def writeDashboard(self):
-        dashboard = self.__createDashboard()
+    def writeDashboard(self, time):
+        dashboard = self.__createDashboard(time)
         print_dashboard(dashboard)
+
+    def get_dashboard_json(self, dashboard, overwrite=False, message="Updated by grafanlib"):
+        '''
+        get_dashboard_json generates JSON from grafanalib Dashboard object
+
+        :param dashboard - Dashboard() created via grafanalib
+        '''
+
+        # grafanalib generates json which need to pack to "dashboard" root element
+        return json.dumps(
+            {
+                "dashboard": dashboard.to_json_data(),
+                "overwrite": overwrite,
+                "message": message
+            }, sort_keys=True, indent=2, cls=DashboardEncoder)
+
+
+    def upload_to_grafana(self, start, end, server, verify=True, username="admin", password="admin"):
+        '''
+        upload_to_grafana tries to upload dashboard to grafana and prints response
+
+        :param time - grafanalib time object
+        :param server - grafana server name
+        :param api_key - grafana api key with read and write privileges
+        '''
+        dashboard_json = self.get_dashboard_json(self.__createDashboard(Time(start, end)), overwrite=True)
+
+        headers = {'Content-Type': 'application/json'}
+        r = requests.post(f"http://{username}:{password}@{server}/api/dashboards/db", data=dashboard_json, headers=headers, verify=verify)
+        # TODO: add error handling
+        print(f"{r.status_code} - {r.content}")
 
